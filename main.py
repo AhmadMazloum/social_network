60910c65acb36839e8a03b1eab81ceb81881795b
ahmadmazloum <ahmadmazloum98@gmail.com>
2022-05-01 14:56:14 +0200

    adding some end points

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import re
-from fastapi import FastAPI
+from turtle import title
+from certifi import contents
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message" : "Hello World!"}
-    
+
+@app.get("/posts")
+async def get_posts():
+    return{"posts" : "total_posts"}
+
+@app.post("/create_post")
+async def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post" : f"title: {payload['title']} content: {payload['content']}"}
